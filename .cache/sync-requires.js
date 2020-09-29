684123f96126ad74e510ae0d0d956691a2d35d18
Raisler Voigt <raisler.voigt777@gmail.com>
2020-09-28 22:45:17 -0300

    Atualizado

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/raisler/Documents/Blog/.cache/dev-404-page.js"))),
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/home/raisler/Documents/Blog/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
   "component---src-templates-categories-list-template-js": hot(preferDefault(require("/home/raisler/Documents/Blog/src/templates/categories-list-template.js"))),
   "component---src-templates-category-template-js": hot(preferDefault(require("/home/raisler/Documents/Blog/src/templates/category-template.js"))),
   "component---src-templates-index-template-js": hot(preferDefault(require("/home/raisler/Documents/Blog/src/templates/index-template.js"))),
